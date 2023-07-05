d0d193c1f4b80bb2b64f06be6d570de4c6f920a9
Daniel adjei <dadjei@quantumgroupgh.com>
2023-07-05 15:44:49 +0000

    change font to raleway

--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -9,7 +9,7 @@
 
   webFontLoader.load({
     google: {
-      families: ['Poppins:100,300,400,500,700,900&display=swap'],
+      families: ['Raleway:100,300,400,500,700,900&display=swap'],
     },
   })
 }
