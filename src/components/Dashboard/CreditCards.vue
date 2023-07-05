348f3f7cc27f9dd1a6a8e866664b8ca5a67ddb2b
Daniel adjei <dadjei@quantumgroupgh.com>
2023-07-05 10:49:12 +0000

    new component for credit cards

---
+++ src/components/Dashboard/CreditCards.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="d-flex justify-space-between align-center">
+        <p class="font-weight-medium">Cards</p>
+        <v-btn variant="outlined" color="grey-lighten-1" size="small" rounded="pill">
+            Add Card
+        </v-btn>
+    </div>
+
+    <!-- Card slider -->
+    <div class="mt-3">
+        <swiper :modules="[Scrollbar]" :slides-per-view="2" :space-between="20">
+            <swiper-slide class="">
+                <v-card variant="flat" color="grey" class="py-7 px-8 mr-2" rounded="xl">
+                    <div class="d-flex justify-space-between">
+                        <p class="text-h5 font-weight-medium">Visa</p>
+                        <v-icon icon="mdi-dots-vertical" />
+                    </div>
+                    <div class="d-flex justify-space-between align-center mt-16">
+                        <p class="text-h5 font-weight-bold">$11,234.00</p>
+                        <div class="text-right text-subtitle-2">
+                            <p>*****4394</p>
+                            <p>05/24</p>
+                        </div>
+                    </div>
+                </v-card>
+            </swiper-slide>
+            <swiper-slide>
+                <v-card variant="flat" color="amber-darken-2" class="py-7 px-8 mr-2" rounded="xl">
+                    <div class="d-flex justify-space-between">
+                        <p class="text-h5 font-weight-medium">Master</p>
+                        <v-icon icon="mdi-dots-vertical" />
+                    </div>
+                    <div class="d-flex justify-space-between align-center mt-16">
+                        <p class="text-h5 font-weight-bold">$11,234.00</p>
+                        <div class="text-right text-subtitle-2">
+                            <p>*****4394</p>
+                            <p>05/24</p>
+                        </div>
+                    </div>
+                </v-card>
+            </swiper-slide>
+            <swiper-slide>
+                <v-card variant="flat" color="blue-darken-2" class="py-7 px-8 mr-2" rounded="xl">
+                    <div class="d-flex justify-space-between">
+                        <p class="text-h5 font-weight-medium">Paypal</p>
+                        <v-icon icon="mdi-dots-vertical" />
+                    </div>
+                    <div class="d-flex justify-space-between align-center mt-16">
+                        <p class="text-h5 font-weight-bold">$11,234.00</p>
+                        <div class="text-right text-subtitle-2">
+                            <p>*****4394</p>
+                            <p>05/24</p>
+                        </div>
+                    </div>
+                </v-card>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Scrollbar } from 'swiper/modules'
+import { SwiperSlide, Swiper } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/scrollbar';
+
+</script>
