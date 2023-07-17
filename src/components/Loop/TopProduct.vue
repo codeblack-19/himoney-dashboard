b23376adaf240e9b77763a357ea68254950d8126
Daniel adjei <dadjei@quantumgroupgh.com>
2023-07-17 23:07:46 +0000

    create top product component

---
+++ src/components/Loop/TopProduct.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-sheet rounded="lg" class="pa-4">
+            <div class="d-flex">
+                <div class="flex-1-0">
+                    <p class="d-flex mb-n1">
+                        <span style="font-size: 20px" class="font-weight-medium mr-2">Top Product</span>
+                    </p>
+                    <p class="text-grey text-body-2">
+                        of the week based on total purchase
+                    </p>
+                </div>
+                <v-btn color="primary" size="small" variant="text">view more</v-btn>
+            </div>
+        </v-sheet>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
