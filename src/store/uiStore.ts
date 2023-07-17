950c1ccbc702d7bc929859c998d39b9fa8291457
Daniel adjei <dadjei@quantumgroupgh.com>
2023-07-17 17:49:18 +0000

    create store for window operations

---
+++ src/store/uiStore.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import { useWindowSize, useWindowScroll } from '@vueuse/core';
+import { ref, computed, watch } from "vue";
+
+export const useUiStore = defineStore("uiStore", () => {
+    const { width: winWidth, height: winHeight  } = useWindowSize();
+    const { y: yScrollWidth } = useWindowScroll();
+    const drawer = ref<boolean>(true);
+    const permanentDrawer = ref<boolean>(true);
+    const winResizeWidth = ref<number>(1280);
+    const showToggle = computed(() => {
+        return winWidth.value < winResizeWidth.value
+    })
+
+    watch(winWidth, (newWidth) => {
+        if (newWidth) {
+            permanentDrawer.value = !showToggle.value
+            if (showToggle.value) {
+                drawer.value = false
+            }
+        }
+    })
+
+    return {
+        winHeight,
+        winWidth,
+        drawer,
+        permanentDrawer,
+        yScrollWidth,
+        showToggle,
+        winResizeWidth
+    }
+})
