9824c4b648475433c25bcb1282e6c2e28b98b81e
Daniel adjei <dadjei@quantumgroupgh.com>
2023-07-18 18:17:52 +0000

    add ads box

--- src/views/Loop.vue
+++ src/views/Loop.vue
@@ -21,12 +21,18 @@
             <v-col cols="12" md="5">
                 <TopTransaction />
             </v-col>
-            <v-col cols="12" md="4">
+            <v-col cols="12" md="4" align-self="stretch">
                 <TopProduct />
             </v-col>
             <v-col cols="12" md="3">
-                <v-sheet rounded="lg" class="pa-2">
-
+                <v-sheet rounded="lg" height="100%" class="addBox py-6 px-6 d-flex flex-column">
+                    <p class="text-white text-h5">
+                        See more detail statistic to analyze your decision.
+                    </p>
+                    <v-spacer></v-spacer>
+                    <p>
+                        <v-btn rounded="lg" class="text-primary font-weight-medium">See More</v-btn>
+                    </p>
                 </v-sheet>
             </v-col>
         </v-row>
@@ -43,3 +49,10 @@ import CustomerChart from "@/components/Loop/Charts/CustomerChart.vue";
 import TopTransaction from "@/components/Loop/TopTransaction.vue";
 import TopProduct from "@/components/Loop/TopProduct.vue";
 </script>
+
+<style>
+.addBox{
+    background: linear-gradient(rgba(0,0,0,0.400), rgba(0, 0, 0, 0.755)), url(../assets/loop/bgImage.webp);
+    background-size: cover;
+}
+</style>
